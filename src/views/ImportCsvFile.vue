ce4ff11d1ae5be8659d6d066ca495ff8ed0f8b0f
Xavier Carol <xcarolr@gmail.com>
2024-03-26 13:15:15 +0100

    first import file skeleton

---
+++ src/views/ImportCsvFile.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card>
+    <v-card-title>
+      {{ $t('import_csv.title') }}
+    </v-card-title>
+    <v-card-text>
+      {{ selectedFilename }}
+    </v-card-text>
+    <v-card-actions>
+      <v-btn>{{ $t('import_csv.import_button') }}</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script setup>
+import { useI18n } from 'vue-i18n';
+
+const importFilename = '';
+const { t: $t } = useI18n();
+const selectedFilename = importFilename.length ? importFilename : $t('import_csv.no_file_selected');
+</script>
