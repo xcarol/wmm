46b05a7f04d77d436d0fc5efc680be7128db298a
Xavier Carol <xcarolr@gmail.com>
2024-03-27 19:30:06 +0100

    read content of selected file

--- src/views/ImportCsvFile.vue
+++ src/views/ImportCsvFile.vue
@@ -5,20 +5,33 @@
     </v-card-title>
     <v-card-actions>
       <v-file-input
+        v-model="importFilename"
         :label="$t('import_csv.label')"
-        :model-value="importFilename"
       >
       </v-file-input>
     </v-card-actions>
     <v-card-actions>
-      <v-btn>{{ $t('import_csv.button') }}</v-btn>
+      <v-btn @click.stop="openSelectedFile">{{ $t('import_csv.open_button') }}</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 
-const importFilename = [];
+const importFilename = ref([]);
 const { t: $t } = useI18n();
+
+const openSelectedFile = () => {
+  const files = importFilename.value;
+  if (files.length) {
+    const reader = new FileReader();
+
+    reader.readAsText(this.chosenFile);
+    reader.onload = () => {
+      this.data = reader.result;
+    };
+  }
+};
 </script>
