d9ae5a5922190e7dc1cad3910168aca4c50dcac0
xcarol <xcarolr@gmail.com>
2024-06-03 15:05:58 +0200

    select and read file content

--- client/src/views/ImportView.vue
+++ client/src/views/ImportView.vue
@@ -1,9 +1,11 @@
 <template>
   <v-container>
-    <v-text-field :label="fileName" />
-    <v-btn variant="outlined">
-      {{ $t('importView.selectFileButton') }}
-    </v-btn>
+    <v-file-input
+      :label="fileName"
+      accept="text/csv"
+      variant="outlined"
+      @change="handleFileChange"
+    />
   </v-container>
   <v-container>
     <v-combobox
@@ -33,8 +35,26 @@ import { useI18n } from 'vue-i18n';
 
 const { t: $t } = useI18n();
 
-const fileName = ref('');
+const fileName = ref($t('importView.importFileInputLabel'));
 const dateItems = ref([]);
 const descriptionItems = ref([]);
 const amountItems = ref([]);
+
+const readFileContent = (file) => {
+  const reader = new FileReader();
+  reader.onload = (event) => {
+    const fileContent = event.target.result;
+    // Processa el contingut del fitxer
+    console.log(fileContent);
+  };
+  reader.readAsText(file);
+};
+
+const handleFileChange = (event) => {
+  const selectedFile = event.target.files[0];
+  if (selectedFile) {
+    readFileContent(selectedFile);
+  }
+};
+
 </script>
