385d2663ba47fef11d87c949f834ed0368ff612c
xcarol <xcarolr@gmail.com>
2024-06-07 17:09:36 +0200

    add sqlview

---
+++ client/src/views/SqlView.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container class="text-end">
+    <v-textarea :label="$t('sqlView.queryAreaLabel')"></v-textarea>
+    <v-btn>BOTO</v-btn>
+  </v-container>
+</template>
+
+<script setup>
+</script>
