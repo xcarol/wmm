b6d2232180f9627308f41b299b985d8df2a1ecb0
xcarol <xcarolr@gmail.com>
2024-06-19 21:25:08 +0200

    get categories

--- client/src/views/FiltersView.vue
+++ client/src/views/FiltersView.vue
@@ -6,13 +6,33 @@
           <v-card>
             <v-card-title>{{ $t('filtersView.categoriesLabel') }}</v-card-title>
             <v-card-text>
-              <v-table></v-table>
+              <v-table>
+                <v-data-table
+                  v-model="selectedCategories"
+                  :items="tableCategories"
+                  show-select
+                  class="elevation-1"
+                  item-key="name"
+                ></v-data-table>
+              </v-table>
             </v-card-text>
             <v-card-actions>
               <v-spacer />
-              <v-btn>{{ $t('filtersView.renameCategoryButton') }}</v-btn>
-              <v-btn>{{ $t('filtersView.deleteCategoryButton') }}</v-btn>
-              <v-btn>{{ $t('filtersView.applyCategoryButton') }}</v-btn>
+              <v-btn
+                :disabled="manySelected"
+                @click.stop="renameCategory"
+                >{{ $t('filtersView.renameCategoryButton') }}</v-btn
+              >
+              <v-btn
+                :disabled="noneSelected"
+                @click.stop="deleteCategories"
+                >{{ $t('filtersView.deleteCategoryButton') }}</v-btn
+              >
+              <v-btn
+                :disabled="noneSelected"
+                @click.stop="applyCategories"
+                >{{ $t('filtersView.applyCategoryButton') }}</v-btn
+              >
             </v-card-actions>
           </v-card>
         </v-col>
@@ -20,7 +40,13 @@
           <v-card>
             <v-card-title>{{ $t('filtersView.filtersLabel') }}</v-card-title>
             <v-card-text>
-              <v-table></v-table>
+              <v-data-table
+                v-model="selectedFilters"
+                :items="tableFilters"
+                show-select
+                class="elevation-1"
+                item-key="name"
+              ></v-data-table>
             </v-card-text>
             <v-card-actions>
               <v-spacer />
@@ -35,4 +61,56 @@
   </v-card>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed, onBeforeMount, onBeforeUpdate, ref } from 'vue';
+import { useApi } from '../plugins/api';
+import { useAppStore } from '../stores/app';
+
+const api = useApi();
+const appStore = useAppStore();
+
+const selectedCategories = ref([]);
+const tableCategories = ref([]);
+const selectedFilters = ref([]);
+const tableFilters = ref([]);
+
+const noneSelected = computed(() => {
+  return selectedCategories.value.length === 0;
+});
+
+const manySelected = computed(() => {
+  return selectedCategories.value.length !== 1;
+});
+
+const listToTable = (list) => {
+  const table = [];
+  list.forEach((category) => {
+    table.push({ id: category });
+  });
+  return table;
+};
+
+const getCategories = async () => {
+  try {
+    const dbNames = await api.categoryNames();
+    tableCategories.value = listToTable(dbNames.data);
+  } catch (e) {
+    appStore.alertMessage = api.getErrorMessage(e);
+  }
+};
+
+const renameCategory = () => {
+
+};
+
+const deleteCategories = () => {
+
+};
+
+const applyCategories = () => {
+
+};
+
+onBeforeUpdate(() => getCategories());
+onBeforeMount(() => getCategories());
+</script>
