ab1edc5b344348170b5972ca1ee35e2c2f7fd2ae
xcarol <xcarolr@gmail.com>
2024-07-18 18:12:50 +0200

    add history to sql command view

--- client/src/stores/app.js
+++ client/src/stores/app.js
@@ -5,14 +5,38 @@ import { useCsvFile } from '../plugins/csvfile';
 export const useAppStore = defineStore('app', {
   state: () => ({
     categorySearchHistory: ref([]),
+    sqlHistory: ref([]),
     alertMessage: ref(''),
     csvfile: useCsvFile(),
   }),
   actions: {
     addSearchToCategoryHistory(search) {
-      if(this.categorySearchHistory.includes(search) === false) {
+      if(search.length && this.categorySearchHistory.includes(search) === false) {
         this.categorySearchHistory.push(search);
       }
+    },
+    addQueryToSqlHistory(query) {
+      if(query.length && this.sqlHistory.includes(query) === false) {
+        this.sqlHistory.push(query);
+      }
+    },
+    nextQueryInHistory(query) {
+      const idx = this.sqlHistory.indexOf(query);
+      
+      if (idx >= 0 && idx < this.sqlHistory.length - 1) {
+        return this.sqlHistory.at(idx + 1);
+      }
+
+      return null;
+    },
+    previousQueryInHistory(query) {
+      const idx = this.sqlHistory.indexOf(query);
+      
+      if (idx > 0) {
+        return this.sqlHistory.at(idx - 1);
+      }
+
+      return null;
     },
   },
 });
