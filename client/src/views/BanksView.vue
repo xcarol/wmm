376fa63768085dabe02e1fd16d6adc65d4bde872
xcarol <xcarolr@gmail.com>
2024-10-02 16:54:11 +0200

    show banks list

--- client/src/views/BanksView.vue
+++ client/src/views/BanksView.vue
@@ -1,17 +1,36 @@
 <template>
   <v-card>
     <v-card-title>BANKS</v-card-title>
+    <v-card-text>
+      <v-list>
+        <v-input>Folders</v-input>
+
+        <v-list-item
+          v-for="bank in banks"
+          :key="bank.id"
+          :prepend-avatar="bank.logo"
+          :title="bank.name"
+        >
+        </v-list-item>
+
+      </v-list>
+    </v-card-text>
   </v-card>
 </template>
 
 <script setup>
-import { onBeforeMount, onBeforeUpdate } from 'vue';
+import { ref, onBeforeMount, onBeforeUpdate } from 'vue';
 import { useApi } from '../plugins/api';
 
 const api = useApi();
+const banks = ref([]);
 
 onBeforeUpdate();
 onBeforeMount(async () => {
-  await api.bankInstitutions();
+  const { data: institutions } = await api.bankInstitutions();
+  institutions.forEach((institution) => {
+    const { id, logo, name } = institution;
+    banks.value.push({ id, logo, name });
+  });
 });
 </script>
