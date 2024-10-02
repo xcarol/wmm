8174a9a5a0c69133c4446b2eed3cb62b74b3c699
xcarol <xcarolr@gmail.com>
2024-10-02 19:00:14 +0200

    format and filter banks

--- client/src/views/BanksView.vue
+++ client/src/views/BanksView.vue
@@ -1,31 +1,60 @@
 <template>
-  <v-card
-    class="flex-grow-1 d-flex flex-column"
-    style="height: 100%; margin: 0; padding: 0; box-sizing: border-box"
+  <v-row
+    class="flex-grow-1 d-flex flex-column my-box"
   >
-    <v-list
-      class="flex-grow-1"
-      style="overflow-y: auto; height: 100%; margin: 0; padding: 0; box-sizing: border-box"
+    <v-col
+      cols="6"
+      class="flex-grow-1 d-flex flex-column my-box"
     >
-      <v-input>Folders</v-input>
-
-      <v-list-item
-        v-for="bank in banks"
-        :key="bank.id"
-        :prepend-avatar="bank.logo"
-        :title="bank.name"
+      <v-card
+        class="flex-grow-1 d-flex flex-column mybox"
       >
-      </v-list-item>
-    </v-list>
-  </v-card>
+        <v-card-title>Select bank to configure</v-card-title>
+        <v-text-field
+          class="pl-4"
+          :model-value="filter"
+          @update:model-value="updateFilter"
+        ></v-text-field>
+        <v-list
+          class="flex-grow-1"
+          style="overflow-y: auto; height: 100%; margin: 0; padding: 0; box-sizing: border-box"
+        >
+          <v-list-item
+            v-for="bank in filteredBanks"
+            :key="bank.id"
+            :prepend-avatar="bank.logo"
+            :title="bank.name"
+            style="cursor: pointer"
+            @click.stop="selectBank"
+          >
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script setup>
-import { ref, onBeforeMount, onBeforeUpdate } from 'vue';
+import { computed, ref, onBeforeMount, onBeforeUpdate } from 'vue';
 import { useApi } from '../plugins/api';
 
 const api = useApi();
 const banks = ref([]);
+
+const filter = ref('');
+
+const updateFilter = (newFilter) => {
+  filter.value = newFilter;
+};
+
+const filteredBanks = computed(() => {
+  const regex = new RegExp(`.*${filter.value}.*`, 'i');
+  return banks.value.filter((bank) => {
+    return regex.test(bank.name);
+  });
+});
+
+const selectBank = () => {};
 
 onBeforeUpdate();
 onBeforeMount(async () => {
@@ -36,3 +65,12 @@ onBeforeMount(async () => {
   });
 });
 </script>
+
+<style scoped>
+.my-box {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
