078125420d4a6e5d77c1fea43d5d4baf4fd39274
xcarol <xcarolr@gmail.com>
2024-12-06 19:24:42 +0100

    retrieve the api url from express in case we are in production mode

--- client/src/main.js
+++ client/src/main.js
@@ -8,14 +8,25 @@ import i18n from './plugins/i18n';
 import vuetify from './plugins/vuetify';
 import api from './plugins/api';
 
-const app = createApp(App);
+(async () => {
+  // Retrieve api url variables from express (../server.cjs) in Production mode
+  const { VITE_API_URL } = await fetch('/env')
+    .then((response) => response.json())
+    .catch(() => {
+      return {};
+    });
 
-app.use(createPinia());
-app.use(VueCookies);
-app.use(router);
-app.use(i18n);
-app.use(vuetify);
-app.use(axios);
-app.use(api, { axios: app.config.globalProperties.axios });
+  const app = createApp(App);
 
-app.mount('#app');
+  app.config.globalProperties.VITE_API_URL = VITE_API_URL;
+
+  app.use(createPinia());
+  app.use(VueCookies);
+  app.use(router);
+  app.use(i18n);
+  app.use(vuetify);
+  app.use(axios);
+  app.use(api, { axios: app.config.globalProperties.axios });
+
+  app.mount('#app');
+})();
