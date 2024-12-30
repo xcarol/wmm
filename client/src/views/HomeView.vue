4630e06754c605eed0d1e289357438332f33b2e7
xcarol <xcarolr@gmail.com>
2024-12-30 18:00:42 +0100

    modify HomeView to display bank balances

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,12 +1,16 @@
 <template>
   <v-card>
-    <v-card-title>Welcome to Where's My Money (wmm)</v-card-title>
-    <v-card-text
-      >Here should be an explanation of the application, or something more interesting than this
-      message.<br />
-      But here we are, and this text's still here...<br />
-      Perhaps... Someday... Who knows... Maybe...</v-card-text
-    >
+    <v-card-title>{{ $t('browseTransactionsView.bankOverview') }}</v-card-title>
+    <v-card-text>
+      <v-data-table
+        :items="banksBalances"
+        :headers="headerBanks"
+        class="elevation-1"
+        fixed-header
+        disable-sort
+        hide-default-footer
+      />
+    </v-card-text>
   </v-card>
 </template>
 
@@ -15,5 +19,69 @@ import {
   VCard,
   VCardText,
   VCardTitle,
+  VDataTable,
 } from 'vuetify/lib/components/index.mjs';
+import { onBeforeMount, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { useProgressDialogStore } from '../stores/progressDialog';
+import { useApi } from '../plugins/api';
+import { useAppStore } from '../stores/app';
+
+const { t: $t } = useI18n();
+const progressDialog = useProgressDialogStore();
+const api = useApi();
+const appStore = useAppStore();
+
+const banksBalances = ref([]);
+
+const headerBanks = [
+  { title: $t('browseTransactionsView.bankNameLabel'), key: 'bank' },
+  { title: $t('browseTransactionsView.amountLabel'), key: 'balance' },
+  { title: $t('browseTransactionsView.dateLabel'), key: 'date' },
+];
+
+const getBanksBrief = async () => {
+  let totalBanksAmount = 0.0;
+
+  progressDialog.startProgress({
+    steps: 0,
+    description: $t('progress.retrievingBanks'),
+  });
+
+  try {
+    const { data: banks } = await api.banksNames();
+
+    const allBalancePromises = [];
+    for (let bankCount = 0; bankCount < banks.length; bankCount += 1) {
+      allBalancePromises.push(api.bankBalance(banks.at(bankCount)));
+    }
+
+    const retrievedBalances = await Promise.all(allBalancePromises);
+    retrievedBalances.forEach(async (result) => {
+      const { data: bankBalance } = result;
+
+      banksBalances.value.push({
+        bank: bankBalance.bank,
+        balance: bankBalance.balance.toFixed(2),
+        date: new Date(bankBalance.latest_date).toLocaleDateString(),
+      });
+
+      totalBanksAmount += bankBalance.balance;
+    });
+
+    banks.splice(0, 0, '');
+    banksBalances.value.push({
+      bank: $t('browseTransactionsView.totalAmount'),
+      balance: totalBanksAmount.toFixed(2),
+    });
+  } catch (e) {
+    appStore.alertMessage = api.getErrorMessage(e);
+  }
+
+  progressDialog.stopProgress();
+};
+
+onBeforeMount(() => {
+  getBanksBrief();
+});
 </script>
